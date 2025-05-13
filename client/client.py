2f00b62ca62bee5bb162ea8fccd2a3533dd99666
kento-ix <kentoix1105@gmail.com>
2025-05-13 15:01:40 -0700

    set up client and server file

---
+++ client/client.py
@@ -0,0 +1,63 @@
+import socket
+import sys
+import threading
+
+def protocol_header(username_len):
+    return int.to_bytes(username_len, 1, "big")
+
+def print_message(bytes_data: bytes):
+    # take out first 1 bit which represent user name length convert as int
+    user_name_len = int.from_bytes(bytes_data[:1], "big")
+
+    user_name = bytes_data[1:user_name_len + 1].decode()
+    message = bytes_data[user_name_len + 1:].decode()
+    print(f"{user_name}: {message}")
+
+def receive_message(sock: socket):
+    while True:
+        response_data = sock.recv(4096)
+        print("\033[2K\r", end="")
+        print_message(response_data)
+
+def input_message(sock: socket, user_name: str):
+    while True:
+        message = input(f"{user_name} :> ")
+        print("\033[1A\033[2K", end="")
+        print(f"{user_name} : {message}")
+        data = protocol_header(len(user_name)) + (user_name + message).encode()
+        sock.send(data)
+        sock.settimeout(2)
+
+def main():
+    # AF_INET allow to use IPv4 to communicate with a remote machine
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # address listen all available network
+    server_address = '0.0.0.0'
+    server_port = 8000
+
+    try:
+        sock.connect((server_address, server_port))
+    except socket.error as err:
+        print(err)
+        sys.exit(1)
+
+    user_name = input('Enter username: ')
+    print(f"{user_name} has joined the chat")
+
+    # send user name information
+    sock.send(protocol_header(len(user_name)) + (user_name).encode())
+
+    # daemon thread when main prigram finish thread automatically exit
+    input_message_thread =  threading.Thread(target=input_message, args=(sock, user_name, ), daemon=True)
+    receive_message_thread =  threading.Thread(target=receive_message, args=(sock, ), daemon=True)
+
+    # prevent to block thread which use input()
+    input_message_thread.start()
+    receive_message_thread.start()
+
+    # stop threads till finish main function
+    input_message_thread.join()
+    receive_message_thread.join()
+
+if __name__ == "__main__":
+    main()
---
+++ server/server.py
@@ -0,0 +1,45 @@
+import socket
+
+
+def main():
+
+    # AF_INET allow to use IPv4 to communicate with a remote machine
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # address listen all available network
+    server_address = '0.0.0.0'
+    server_port = 8000
+    print(f"Starting up on port {server_port}")
+    sock.bind((server_address, server_port))
+
+    clients = []
+
+    while True:
+        try:
+            data, address = sock.recvfrom(4096)
+            if not data:
+                continue
+
+            # add client if not exit
+            if address not in clients:
+                clients.append(address)
+            
+            # take out user name from first byte
+            user_name_len = int.from_bytes(data[:1], "big")
+            # ignore first 1 byte take out latter name data
+            user_name = data[1: user_name_len + 1].decode()
+            # ignore first usernmae length and take out latter massge data
+            message = data[user_name_len + 1:].decode()
+            print("Username: ", user_name)
+            print("Message: ", message)
+
+            # broad cast message 
+            for client in clients:
+                if client != address:
+                    sock.sendto(data, client)
+
+        except KeyboardInterrupt:
+            print("\nServer stopped.")
+            break
+
+if __name__ == "__main__":
+    main()
